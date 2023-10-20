48b5ebec117019629947c9ac32f1517b49e16b86
cod-emminex <emminexy@yahoo.com>
2023-10-20 17:02:22 +0000

    new update

--- tests/main.c
+++ tests/main.c
@@ -1,25 +1,47 @@
 #include "emmy.h"
 
 /**
- * main - Entry point
- *
- * Return: 0
- */
-
+    * main - Entry point
+    * @ac: Number of arguments
+    * @av: Array of arguments
+    * @env: Array of environment variables
+    *
+    * Return: Always 0 (Success)
+    */
 int main(void)
 {
-	char format[1024];
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char **args;
 
-	while (1)
+    while (1)
 	{
-		emmyf(format);
+		display_prompt();
+        read = getline(&line, &len, stdin);
+        if (read == -1) {
+            exit(EXIT_SUCCESS);
+        }
 
-	/**
-	 * display_prompt();
-	 * read_command(command, sizeof(command));
-	 * execute_command(command);
-	 */
+        /* Parse the command line into arguments */
+        args = parse_line(line);
+        if (args == NULL)
+        {
+            my_printf("Error: Invalid command\n");
+            continue;
+        }
 
-	}
-	return (0);
+        /* Execute the command */
+        execute_command(args[0]);
+
+        /* Free the memory allocated for the arguments */
+        free(args);
+    }
+	free(line);
+    return (0);
 }
+
+
+
+
+
